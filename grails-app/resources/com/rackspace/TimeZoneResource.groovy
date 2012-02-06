b297323412cd5bd966e56fd02ae635d0df0c77db
Aaron Brown <aaron.brown@rackspace.com>
2012-02-06 11:57:33 -0600

    Completed the resource

--- grails-app/resources/com/rackspace/TimeZoneResource.groovy
+++ grails-app/resources/com/rackspace/TimeZoneResource.groovy
@@ -1,1 +1,26 @@
 package com.rackspace
+
+// Imports
+import javax.ws.rs.Path
+import javax.ws.rs.Consumes
+import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
+import javax.ws.rs.Get
+
+import static org.grails.jaxrs.response.Responses.*
+
+@consumes(['application/xml','application/json'])
+@produces(['application/xml','application/json'])
+
+@Path('/api/tz')
+
+class TimeZoneResource
+{
+    @GET
+    @PATH('/')
+    Response returnCurrentTime(@QueryParam('timezone') String timezone)
+    {
+        def result = TimeZoneService.getTime(timezone)
+        
+        ok result
+    }
